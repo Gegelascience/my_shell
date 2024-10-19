98fa697bafdfa85fc9ae54c1a670a4bab94dc7ff
VERCHERE Rémi <remi.verchere2@gmail.com>
2024-10-19 22:18:49 +0200

    ajout du pipe

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ use std::io::Write;
 use std::path::Path;
 use std::env;
 use std::process::Command;
+use std::process::Stdio;
+use std::process::Child;
 
 fn main() {
     loop {
@@ -14,34 +16,64 @@ fn main() {
         stdin().read_line(&mut input).unwrap();
 
         // recuperation de la commande et des parametres avec trim
-        let mut parts = input.trim().split_whitespace();
-        let command = parts.next().unwrap();
-        let args = parts;
+        
 
+        let mut commands = input.trim().split(" | ").peekable();
+        let mut previous_command = None;
 
-        match command {
-            "cd" => {
-                // backup /
-                let new_dir = args.peekable().peek().map_or("/", |x| *x);
-                let root = Path::new(new_dir);
-                if let Err(e) = env::set_current_dir(&root) {
-                    eprintln!("{}", e);
+        while let Some(command) = commands.next()  {
+            let mut parts = command.trim().split_whitespace();
+            let command = parts.next().unwrap();
+            let args = parts;
+
+            match command {
+                "cd" => {
+                    // backup /
+                    let new_dir = args.peekable().peek().map_or("/", |x| *x);
+                    let root = Path::new(new_dir);
+                    if let Err(e) = env::set_current_dir(&root) {
+                        eprintln!("{}", e);
+                    }
+                },
+                "exit" => return,
+                command => {
+                    let stdin = previous_command
+                        .map_or(
+                            Stdio::inherit(),
+                            |output: Child| Stdio::from(output.stdout.unwrap())
+                        );
+
+                    let stdout = if commands.peek().is_some() {
+                        // il y a une commande supplementaire il traiter
+                        Stdio::piped()
+                    } else {
+                        // traitement derniere commande
+                        Stdio::inherit()
+                    };
+
+                    let output = Command::new(command)
+                        .args(args)
+                        .stdin(stdin)
+                        .stdout(stdout)
+                        .spawn();
+
+                    match output {
+                        Ok(output) => { previous_command = Some(output); },
+                        Err(e) => {
+                            previous_command = None;
+                            eprintln!("{}", e);
+                        },
+                    };
                 }
-            },
-            "exit" => return,
-            command => {
-                let child = Command::new(command)
-                    .args(args)
-                    .spawn();
-
-
-                // gestion d'une commande inconnue
-                match child {
-                    Ok(mut child) => { child.wait(); },
-                    Err(e) => eprintln!("{}", e),
-                };
             }
-        }         
+        }
+
+        if let Some(mut final_command) = previous_command {
+            // attente resultat derniere commande
+            final_command.wait();
+        }
+
+                 
 
     }
     
